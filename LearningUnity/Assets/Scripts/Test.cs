bcb68fb756d8903ca0b9963e4a765775ce7df3ac
Ankit Jailwal <jailwalankit@gmail.com>
2022-01-22 02:26:59 +0530

    Adding velocity and collision

--- LearningUnity/Assets/Scripts/Test.cs
+++ LearningUnity/Assets/Scripts/Test.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    Rigidbody cube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cube = GetComponent<Rigidbody>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Destroys gameObject on pressing Space
+        
         if(Input.GetKeyDown(KeyCode.Space)){
-            Destroy(gameObject);
+        // Destroys gameObject on pressing Space
+            //Destroy(gameObject);
+        // Adds upwards force to cube
+            cube.AddForce(Vector3.up*200);
+        // Adds Velocity to object
+            cube.velocity = Vector3.forward * 2f;    
         }
     }
 
     private void OnMouseDown(){
-        Destroy(gameObject);
+       // Destroy(gameObject);
+    }
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Enemy"){
+            Destroy(collision.gameObject);
+        }
+            
     }
 }
